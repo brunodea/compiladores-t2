7b885ab3e09cd8cb3292335914fab72a9d6446ef
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-07-02 18:10:19 -0300

    Arrumei diversos problemas no tokens.py
    P.S.: o t_INT,t_FLOAT,t_STRING e o t_IDENTIFIER eu encontrei pronto em um site. :-) (não podemos perder tempo né :-P)

--- tokens.py
+++ tokens.py
@@ -1,12 +1,11 @@
 
 tokens = (
-    'NUMBER','PLUS','MINUS','TIMES','DIVIDE','EQUALS','LPAREN','RPAREN',
+    'PLUS','MINUS','TIMES','DIVIDE','EQUALS','LPAREN','RPAREN',
     'VARIABLES_DECL','COLON', 'SEMICOLON', 'COMMA', 'VAL',
     'INT','FLOAT','BOOLEAN','STRING','LIST', 'CHAR', 'TRUE', 'FALSE',
-    'IF','ELSE','IDENTIFIER'
+    'IF','ELSE','IDENTIFIER','TYPE'
     )
 
-t_VARIABLES_DECL = r'Variables'
 t_PLUS           = r'\+'
 t_MINUS          = r'\*'
 t_DIVIDE         = r'/'
@@ -16,30 +15,71 @@ t_RPAREN         = r'\)'
 t_COLON          = r'\:'
 t_SEMICOLON      = r'\;'
 t_COMMA          = r'\,'
-t_INT            = r'int'
-t_FLOAT          = r'float'
-t_BOOLEAN        = r'boolean'
-t_STRING         = r'string'
-t_LIST           = r'list'
-t_CHAR           = r'char'
-t_TRUE           = r'True'
-t_FALSE          = r'False'
-t_IF             = r'if'
-t_ELSE           = r'else'
-t_VAL            = r'val'
-t_IDENTIFIER     = r'bruno'#[a-zA-Z_][a-zA-Z0-9_]*'
 
 t_ignore         = " \t"
 
-def t_NUMBER(t):
-    r'\d+'
+def t_VARIABLES_DECL(t):
+    r'Variables'
+    return t
+def t_VAL(t):
+    r'val'
+    return t
+def t_IF(t):
+    r'if'
+    return t
+def t_ELSE(t):
+    r'else'
+    return t
+
+def t_FLOAT(t):
+    r'-?\d+\.\d*(e-?\d+)?'
+    t.value = float(t.value)
+    return t
+def t_INT(t):
+    r'-?\d+'
     t.value = int(t.value)
+    return t
+def t_BOOLEAN(t):
+    r'true|false'
+    return t
+def t_STRING(t):
+    r'\"([^\\"]|(\\.))*\"'
+    escaped = 0
+    str = t.value[1:-1]
+    new_str = ""
+    for i in range(0, len(str)):
+        c = str[i]
+        if escaped:
+            if c == "n":
+                c = "\n"
+            elif c == "t":
+                c = "\t"
+            new_str += c
+            escaped = 0
+        else:
+            if c == "\\":
+                escaped = 1
+            else:
+                new_str += c
+    t.value = new_str
+    return t
+def t_CHAR(t):
+    r'\'.?\''
+    t.value = t.value[1:-1]
+    return t
+
+def t_TYPE(t):
+    r'int|float|boolean|string|list|char'
     return t
 
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
-
+    
+def t_IDENTIFIER(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    return t
+    
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
@@ -80,15 +120,11 @@ def p_vars_init(p):
     VARIABLES[p[2]] = Variable(p[1],p[2],p[4])
     
 def p_type(p):
-    '''type : INT 
-            | FLOAT 
-            | BOOLEAN 
-            | STRING 
-            | LIST'''
+    'type : TYPE'
     p[0] = p[1]
 
 def p_expression(p):
-    'expression : NUMBER SEMICOLON'
+    'expression : INT SEMICOLON'
     p[0] = p[1]
     
 def p_error(p):
